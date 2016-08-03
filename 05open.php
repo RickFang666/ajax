f3fc2869b7f24bf15e2661b93eec6aabbcffe9aa
RickFang666 <rickfang0123@gmail.com>
2016-08-04 02:12:47 +0800

    1

---
+++ 05open.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+$username = $_GET['username'];
+$password = $_GET['password'];
+
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+// echo 1;
+
+echo '用户名：'.$username.'密码：'.$password;
+
+?>
